5627cdaecefe39ecb2fd110dafce4f1094178b50
Kashefy <kashefymajid1992@gmail.com>
2026-06-13 01:28:22 +0330

    first commit

--- bot.py
+++ bot.py
@@ -46,6 +46,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     app = Application.builder().token(TOKEN).build()
+    app.run_polling()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
